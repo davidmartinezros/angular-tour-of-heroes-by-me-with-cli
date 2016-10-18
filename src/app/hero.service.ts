5bf838f90e18e1c4a3d810d59bf7800eb0e8cc5e
David <david.martinez.etic@gmail.com>
2016-10-18 16:58:21 +0200

    Make the update of a hero with a save button in the detail of a hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -9,12 +9,9 @@ import { Hero } from './hero';
 export class HeroService {
     
     private heroesUrl = 'app/heroes';  // URL to web api
+    private headers = new Headers({'Content-Type': 'application/json'});
 
     constructor(private http: Http) { }
-    
-    /*getHeroes(): Promise<Hero[]> {
-        return Promise.resolve(HEROES);
-    }*/
 
     getHeroes(): Promise<Hero[]> {
         return this.http.get(this.heroesUrl)
@@ -26,6 +23,15 @@ export class HeroService {
     getHero(id: number): Promise<Hero> {
         return this.getHeroes()
              .then(heroes => heroes.find(hero => hero.id === id));
+    }
+
+    update(hero: Hero): Promise<Hero> {
+        const url = `${this.heroesUrl}/${hero.id}`;
+        return this.http
+            .put(url, JSON.stringify(hero), {headers: this.headers})
+            .toPromise()
+            .then(() => hero)
+            .catch(this.handleError);
     }
 
     private handleError(error: any): Promise<any> {
